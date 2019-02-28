11453fd94c10f357374c2486bc4ec4a38cbe29bf
EC2 Default User <ec2-user@ip-172-31-37-106.eu-west-1.compute.internal>
2019-02-28 17:06:14 +0000

    v3

--- apache_log_parser/parsing_apache.py
+++ apache_log_parser/parsing_apache.py
@@ -12,14 +12,15 @@ def Top10IPs(lines):
     Show the top 10 IPs making the most requests, displaying the IP address and number of requests made for each.
     """
     top10requesting = {}
+    Ip_position = 0
     #we iterate all the lines in the file 
     for line in file_lines:
         splitted=line.split()
     ###we check the ips that are repeated and how many times
-        if (splitted[0]) not in top10requesting:
-            top10requesting[(splitted[0])]=1
+        if (splitted[Ip_position]) not in top10requesting:
+            top10requesting[(splitted[Ip_position])]=1
         else:
-            top10requesting[(splitted[0])]+=1
+            top10requesting[(splitted[Ip_position])]+=1
          
     ###we print the ips in order by adding an inline function that adds the second element ,the value,to be passed to the sort function 
     print ("\n\n########### TOP 10 IPS #############\n")
@@ -41,16 +42,18 @@ def Top5PagesforTop10IPs(top10list,lines):
     For each of the top 10 IPs, this shows the top 5 pages requested and the number of requests for each. (anything 2xx or 3xx)
     """
     top5requesting = {}
+    site_position = 6
+    code_position = -2
 
     for ip in top10list:
         for line in lines:
             if ip in line:          
                 splitted=line.split()
-                if re.match(("^[2-3]"), (splitted[-2])): 
-                    if (splitted[6]) not in top5requesting:
-                        top5requesting[(splitted[6])]=1                 
+                if (int(splitted[code_position]) > 199 and int(splitted[code_position]) < 309): 
+                    if (splitted[site_position]) not in top5requesting:
+                        top5requesting[(splitted[site_position])]=1                 
                     else:
-                        top5requesting[(splitted[6])]+=1                 
+                        top5requesting[(splitted[site_position])]+=1                 
 
                 else :
                     continue
@@ -64,12 +67,13 @@ def Top5PagesforTop10IPs(top10list,lines):
 def Top10pages(lines):
     """ Top 10 requested pages and the number of requests made for each """
     top10sites = {}
+    site_position = 6
     for line in lines:
         splitted=line.split()       
-        if (splitted[6]) not in top10sites:
-            top10sites[(splitted[6])]=1                 
+        if (splitted[site_position]) not in top10sites:
+            top10sites[(splitted[site_position])]=1                 
         else:
-            top10sites[(splitted[6])]+=1                 
+            top10sites[(splitted[site_position])]+=1                 
                
     print (f"\n ########## TOP 10 SITES REQUESTED ########\n")
     i=0
@@ -81,13 +85,15 @@ def Top10pages(lines):
 def Top10Unsuccessful(lines) : 
     """Top 10 unsuccessful page requests"""
     top10fail = {}
+    code_position = -2
+    site_position = 6
     for line in lines:
         splitted=line.split()
-        if re.match(("^[4-5]"), (splitted[-2])):
-            if (splitted[6]) not in top10fail:
-                top10fail[(splitted[6])]=1
+        if re.match(("^[4-5]"), (splitted[code_position])):
+            if (splitted[site_position]) not in top10fail:
+                top10fail[(splitted[site_position])]=1
             else:
-                top10fail[(splitted[6])]+=1
+                top10fail[(splitted[site_position])]+=1
 
         else :
             continue
