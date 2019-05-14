e9eba490c1d2d57eb8676e043ebc1753723ce377
root <root@u3417ebcb493b58d91c06.ant.amazon.com>
2019-05-14 16:41:12 +0100

    v2

--- python_xargs_stdin_osexec/python_xargs_stdin_osexec.py
+++ python_xargs_stdin_osexec/python_xargs_stdin_osexec.py
@@ -2,32 +2,34 @@
 
 receives the input of a csv file and behaves like xargs 
 xargs reads items from the standard input, delimited by blanks and executes the command 
- like :   echo users.csv | python3 myxargs.py useradd -e 2080-03-27
 
-CSV files contain data comma-separated values describing values in a table.:
+echo users.csv | python3 python_xargs_stdin_osexec.py  useradd -e 2080-03-27 -U 
+['useradd', '-e', '2080-03-27', '-U', 'frankriley']
 
-FIRST NAME ,LAST NAME,USERNAME ,PASSWORD ,EMAIL ADDRESS,PHONE NUMBER,PASSPORT,GROUPS,USERCODE,TITLE,ADDRESS 1 ,ADDRESS 2,CITY,STATE,ZIP
-Frank,Riley,friley,changeme,friley@kanab.org,123-456-7890,3,"1,3",1040,Teacher,328 Innovation,Suite # 200 ,state college,PA,16803
-Steve,Brannigan,sbrannigan,changeme,sbrannigan@kanab.org,123-456-7890,3,1,1041,Teacher,328 Innovation,Suite # 200 ,state college,PA,16803
-Marie,Ambrose,mambrose,changeme,mambrose@kanab.org,123-456-7890,3,1,1042,Teacher,328 Innovation,Suite # 200 ,state college,PA,16803
 
 In this example we create only the first  user that shows up in the cxv file in a linux system concatenating first and last name of each user.
-We expect only one input in the stdin, the name of the csv file
+We expect only one input in the stdin, the name of the csv file and an infinite numebr of arguments
 because only use execution is going to be done we use os.execvp which replaces the current program execution. If we needed to add all of the users in the csv file we'd use subprocess.call
 ''' 
 
-import sys,csv,os 
+import sys,csv,os,logging
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
 
 ### we create a string for the command and its possible arguments.
 
+if len(sys.argv) < 1  :
+    print("This program needs at least one argument")
+    sys.exit(1)
+
 command =  sys.argv[1]
 args=[]
-args.append(command)
+#args.append(command)
 
-for i in sys.argv[2:] :     ##create a list excluding the name of the script with the command args
+for i in sys.argv[1:]:
     args.append(i)
-space=" "
-
 
 
 for line in sys.stdin:         ####we get a string with the stdin filename to parse later
@@ -36,18 +38,20 @@ for line in sys.stdin:         ####we get a string with the stdin filename to pa
 user=""
 users = []
 
+try:
 
-with open(filename) as f:      ### we open the file and create csv reader object to go through it
-    reader = csv.reader(f)
-    next(reader)                 ### we forward one line the position of the reader 
-    for row in reader:           
-        print (row)
-        user = row[0]+row[1]  
-        user= user.lower()
-        args.append(user)  
-        os.execvp(command, args)
-        del args[-1]
-
+    with open(filename) as f:      ### we open the file and create csv reader object to go through it
+        reader = csv.reader(f)
+        next(reader)                 ### we forward one line the position of the reader 
+        for row in reader:           
+            user = row[0]+row[1]  
+            user= user.lower()
+            args.append(user)  
+            print(args)
+            os.execvp(command, args)
+            del args[-1]
+except Exception:
+    log.exception("Execution has failed!")
 
 # To mimic xargs behaviour, we execute a Python script without spawning a new process inline within the same interpreter by invoking exec
 
